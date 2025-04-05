33be5efaa4c7f9fc1d532b50e8b4aed069970a84
Beckett Dunning <Beckett2000@gmail.com>
2025-04-05 03:08:39 -0400

    Update object-tests.lua
    Added test for object :indexOf

--- object-tests.lua
+++ object-tests.lua
@@ -17,5 +17,8 @@ testObject = function()
     print(tree:first(5))
     print(tree:last(8))
     print(tree.last:indexOf(490))
+
+    tree.insert:atIndex(50,"nil")
+    print(tree.last:indexOf(490,nil,20,"nil",100,5,80))
     
 end 
