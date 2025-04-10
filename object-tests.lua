a568ea308fde9a2ea8e017cdff8a3d4cccd2f5da
Beckett Dunning <Beckett2000@gmail.com>
2025-04-10 11:08:13 -0400

    Create object-tests.lua

---
+++ object-tests.lua
@@ -0,0 +1,81 @@
+-- Tests for object.lua 
+
+----- ----- ----- ----- ----- -----
+if true then return end
+----- ----- ----- ----- ----- -----
+
+testToString = function()
+    
+    local tree = object{"leaves"}
+    
+    local multiArray = object{"a","b","c",{"d","e","f",{"g","h","i"}},"j","k","l","m",{"n","o","p"},"q","r","s"}
+    
+    print(tree)
+    
+    multiArray.toString:config{
+      offsets = false,
+      indents = " ",
+      lengths = true,
+      depth = 2
+    }
+
+    print(multiArray)
+    
+end
+
+
+testObjectToString = function()
+    
+    local toString = object.toString
+    
+    local multiArray = object{"a","b","c",{"d","e","f",{"g","h","i"}},"j","k","l","m",{"n","o","p"},"q","r","s"}
+    
+    print(multiArray)
+    
+    print(" ------- ---------- ------- ")
+    
+    print(multiArray:toString{
+        offsets = false,
+        depth = 3
+    })
+    
+end
+
+
+-- :first / :last / :range
+testObject = function()
+    
+    local tree = object()
+    
+    for i = 1,100 do 
+        tree:insert(i,i*10) 
+    end
+    
+    print(tree:range(50,20))
+    print(tree:first(5))
+    print(tree:last(8))
+    print(tree.last:indexOf(490))
+
+    tree.insert:atIndex(50,"nil")
+    print(tree.last:indexOf(490,nil,20,"nil",100,5,80))
+    
+end 
+
+testObject = function()
+    
+    local test = object{"a","b","c","d","b","e","a","f"}
+    
+    print(test)
+    
+    print(test:first())
+    print(test.first:indexOf("b","c","a"))
+    print(test:lastIndexOf("b","c","a"))
+    
+    test.insert:atIndex(4,"abc")
+    print(test)
+    
+    test.remove:indexiesOf("a")
+    print(test)
+    
+end
+
