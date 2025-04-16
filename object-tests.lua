f12884fc803ab6579a9a9bae5b44195bf5f3aa85
Beckett Dunning <Beckett2000@gmail.com>
2025-04-16 07:08:47 -0400

    Update object-tests.lua
    added: object.lua test class

--- object-tests.lua
+++ object-tests.lua
@@ -1,10 +1,137 @@
--- Tests for object.lua 
+-------------------------------------------
+-- objectTests.lua - Tests for object.lua 
+-------------------------------------------
+local object = require(asset.object)
+-------------------------------------------
 
------ ----- ----- ----- ----- -----
-if true then return end
------ ----- ----- ----- ----- -----
+-- run tests for the object.lua class
+-- local runTests = require(asset.objectTests)
+-- runTests("*") -- run test(s)
 
-testExtend = function()
+-------------------------------------------
+----- ----- ----- ----- ----- -----
+-- if true then return end -- 
+----- ----- ----- ----- ----- -----
+-------------------------------------------
+
+-------------------------------------------
+local _timestamp -- today: (hh:mm:ss)
+-------------------------------------------
+
+local objectTests = {
+ extend = {}, api = {}, tostring = {}, 
+ print = {}
+}
+
+------------------------------------------->
+-- methods to run tests ...
+------------------------------------------->
+
+local function runTests(categories) 
+  
+  ---------- ------ ---------- ------
+  local concat = table.concat
+  ---------- ------ ---------- ------
+  
+  local header = { concat {
+   "======================== \n", "'objectTests.lua': "},
+   "\n========================" }
+  
+  if not object then
+    print(concat{header[1],
+     "object.lua is missing ...",
+     header[2]}) return
+  end
+  
+  ---------- ------ ---------- ------
+  local concat,tostring,keys = object.concat or table.concat, object.tostring or tostring, object.keys
+  ---------- ------ ---------- ------
+  
+  if not categories then
+    print(concat{header[1],"No category argument to 'runTests'. Possible category options: {", keys(objectTests),"}.",
+     header[2]})
+  end
+  
+  -- run all tests - runTests("*")
+  if categories == "*" then
+   categories = keys(objectTests)
+     
+  -- run selected tests - runTests(category)
+  elseif type(categories) == "string" then categories = {categories} 
+    
+  end
+  
+  ---------- ------ ---------- ------
+  local count = {errors = 0, tests = 0}
+  local spacer = "---- ---- ---- ---- ----"
+  ---------- ------ ---------- ------
+  local testCount,step = 0,1
+  ---------- ------ ---------- ------
+  
+  for i = 1, #categories do
+   local category = categories[i]
+   if objectTests[category] then
+    testCount = testCount + #keys(objectTests[category])
+    end end
+  
+  ---------- ------ ---------- ------
+  
+  local testsWillRun = testCount > 0
+  
+  if not testsWillRun then
+    print(concat{header[1],
+      "No tests to run ...", header[2] })
+   return end
+  
+  print(concat{header[1], 
+   "Running tests for object class ...",
+    "\n -- tests to run: ", testCount,
+   header[2] })
+  
+  ---------- ------ ---------- ------
+  
+  for i = 1, #categories do
+   local category = categories[i]
+   
+   local testLookup = objectTests[category]
+   for key,test in pairs(testLookup) do
+      
+    local testName = 
+     concat{"('",category,"':[",key,'])'}
+      
+    print(concat{spacer.." ","\n:: start test (",step,"/",testCount,") :: ",testName,": ",_timestamp(),"\n",spacer})
+    local status,message = pcall(test)
+      
+    if status == false then 
+     print(concat{spacer.." ","\n:: end test :: ",testName,": ",_timestamp(),' -  test failed with error message: "',message,' - 1"\n',spacer})
+     count.errors = count.errors + 1
+    else 
+      print(concat{spacer.." ","\n::: end test :: ",testName,": ", _timestamp(),' - test finished running. - 0\n',spacer})
+    end  
+      
+    count.tests = count.tests + 1
+    step = step + 1
+      
+   end 
+    
+  end
+  
+  print(concat{
+   "======================== \n", 
+   "|| end of tests ...",
+   "\n--> errors: ",count.errors,
+   " tests: ",count.tests,
+   "\n========================" 
+  })
+
+end
+
+
+------------------------------------------->
+-- test inplementations: (4-15) note: currently the tests are self contained so values are not passed in or returned ... 
+------------------------------------------->
+
+objectTests.extend["4-14-25"] = function()
     
     local plant = object{"a","b","c"}
 
@@ -51,125 +178,9 @@ testExtend = function()
     
 end
 
+------ ------ ------ ------ ------
 
-
-testPrinting = function()
-    
-  local myTable = {"a","b"}
-  setmetatable(myTable,{__tostring =
-   function() return "a b c d e f" end })
-    
-  local tree = object{"leaves","bark",
-  kind = "oak", [function() end] = "foo",["1"]="one",[{"a","b","c"}]= "alpha",
-  ["customTable"] = myTable, [myTable] = "custom"}
-    
-  print(object.toString(tree,{
-    style = "vertical",
-    spacer = " ", 
-    offsets = true,
-    lengths = true,
-    depth = 1
-  }))
-
---[[
-
-output: 
-
-(object[2]: 0x307a7a1c0):{
-  01:"leaves", 
-  02:"bark", 
-  [(table[3]: 0x307a7a6c0)]:"alpha", 
-  ["1"]:"one", 
-  kind:"oak", 
-  [(table[2]: 0x307a78b00: { __tostring = "a b c d e f" })]:"custom", 
-  [(function: 0x30a46b0a0)]:"foo", 
-  customTable:(table[2]: 0x307a78b00: { __tostring = "a b c d e f" })
-}
-
-]]
-
-end
-
-
-
-testPrettyPrint = function()
-
-   ------ ------ ------ ------>>
-
-   local tree = object{"leaves","bark",
-     kind = "oak",["1"]="one",alpha = {"a","b","c"}}
-    
-    print(tree:toString{
-      ---- ---- ---- ----
-      style = "vertical",
-      depth = 2,
-      spacer = "..",
-      ---- ---- ---- ----
-      offsets = true,
-      lengths = true
-      ---- ---- ---- ----
-    })
-
-    ------ ------ ------ ------>>
-
-    --[[ output:
-
-      (object[2]: 0x306f20140):{
-      ....01:"leaves", 
-      ....02:"bark", 
-      ....["1"]:"one", 
-      ....kind:"oak", 
-      ....alpha:(table[3]: 0x306f20bc0):{
-      ......01:"a", 
-      ......02:"b", 
-      ......03:"c"
-      ....}
-      }
-
-    ]]
-
-end
-
-testToString = function()
-
-   local tree = object{"leaves","bark",
-    kind = "oak", [function() end] = "foo",["1"]="one",[{"a","b","c"}]= "alpha"}
-    
-   print(tree) 
-    
-  -- (object[2]: 0x311d87500):{01:"leaves", 02:"bark", [(function: 0x15584d880)]:"foo", kind:"oak", [(table[3]: 0x311d85a00)]:"alpha", ["1"]:"one"}
-    
-  local tree = object{"leaves","bark",
-   kind = "oak", ["1Value"] = "one", ["two"]=20}
-  
-  print(tree) 
-
-  -- (object[2]: 0x311ca5980):{01:"leaves", 02:"bark", kind:"oak", two:20, ["1Value"]:"one"}
-    
-end
-
-
-testToString = function()
-    
-    local tree = object{"leaves"}
-    
-    local multiArray = object{"a","b","c",{"d","e","f",{"g","h","i"}},"j","k","l","m",{"n","o","p"},"q","r","s"}
-    
-    print(tree)
-    
-    multiArray.toString:config{
-      offsets = false,
-      spacer = " ",
-      lengths = true,
-      depth = 2
-    }
-
-    print(multiArray)
-    
-end
-
-
-testObjectToString = function()
+objectTests.tostring["4-10-25"] = function()
     
     local toString = object.toString
     
@@ -186,9 +197,10 @@ testObjectToString = function()
     
 end
 
+------ ------ ------ ------ ------
 
--- :first / :last / :range
-testObject = function()
+-- (04/05/25) :first / :last / :range
+objectTests.api["4-05-25:(1)"] = function()
     
     local tree = object()
     
@@ -200,13 +212,15 @@ testObject = function()
     print(tree:first(5))
     print(tree:last(8))
     print(tree.last:indexOf(490))
-
+    
     tree.insert:atIndex(50,"nil")
     print(tree.last:indexOf(490,nil,20,"nil",100,5,80))
-    
+
 end 
 
-testObject = function()
+------ ------ ------ ------ ------
+
+objectTests.api["4-05-25:(2)"] = function()
     
     local test = object{"a","b","c","d","b","e","a","f"}
     
@@ -224,3 +238,115 @@ testObject = function()
     
 end
 
+
+------ ------ ------ ------ ------
+
+objectTests.print["4-11-25 (printing)"] = function()
+    
+  local myTable = {"a","b"}
+  setmetatable(myTable,{__tostring =
+   function() return "a b c d e f" end })
+    
+  local tree = object{"leaves","bark",
+  kind = "oak", [function() end] = "foo",["1"]="one",[{"a","b","c"}]= "alpha",
+  ["customTable"] = myTable, [myTable] = "custom"}
+    
+  print(object.toString(tree,{
+    style = "vertical",
+    spacer = " ", 
+    offsets = true,
+    lengths = true,
+    depth = 1
+  }))
+
+  --[[
+
+  output: 
+
+  (object[2]: 0x307a7a1c0):{
+    01:"leaves", 
+    02:"bark", 
+    [(table[3]: 0x307a7a6c0)]:"alpha", 
+    ["1"]:"one", 
+    kind:"oak", 
+    [(table[2]: 0x307a78b00: { __tostring = "a b c d e f" })]:"custom", 
+    [(function: 0x30a46b0a0)]:"foo", 
+    customTable:(table[2]: 0x307a78b00: { __tostring = "a b c d e f" })
+  }
+
+  ]]
+
+end
+
+------ ------ ------ ------ ------
+
+objectTests.print["4-11-25 (prettyPrint)"] = function()
+  
+  ------ ------ ------ ------>>
+  
+  local tree = object{"leaves","bark",
+    kind = "oak",["1"]="one",alpha = {"a","b","c"}}
+  
+  print(tree:toString{
+    ---- ---- ---- ----
+    style = "vertical",
+    depth = 2,
+    spacer = "..",
+    ---- ---- ---- ----
+    offsets = true,
+    lengths = true
+    ---- ---- ---- ----
+  })
+  
+  ------ ------ ------ ------>>
+  
+  --[[ output:
+  
+  (object[2]: 0x306f20140):{
+    ....01:"leaves", 
+    ....02:"bark", 
+    ....["1"]:"one", 
+    ....kind:"oak", 
+    ....alpha:(table[3]: 0x306f20bc0):{
+      ......01:"a", 
+      ......02:"b", 
+      ......03:"c"
+    ....}
+  }
+  
+]]
+
+end
+
+
+-------------------------------------------
+-- private methods ...
+-------------------------------------------
+if not object then return runTests end
+-------------------------------------------
+local concat,tostring,keys = object.concat or table.concat, object.tostring or tostring, object.keys
+-------------------------------------------
+
+-- gets a timestamp for logs
+_timestamp = function()
+  
+  local t = os.date("*t")
+  -- print("(os.date)",object.tostring(t))
+  
+  local formatNumber = function(val)
+   local val = tostring(val)
+   if string.len(val) == 1 then val = "0"..val
+   end return val end
+  local ft = formatNumber
+    
+  return concat {
+   "(",ft(t.hour),":",ft(t.min),":",  ft(t.sec),")"
+  } 
+    
+end --> (string) timestamp
+
+-------------------------------------------
+------ ------ ------ ------ ------
+return runTests ------> ----> ------>
+------ ------ ------ ------ ------
+-------------------------------------------
