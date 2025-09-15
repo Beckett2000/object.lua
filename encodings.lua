b53292152df2f4ca9cbdc830cc39db92609b2227
Beckett Dunning <Beckett2000@gmail.com>
2025-09-14 20:30:10 -0400

    Update encodings.lua
    Added a vigenere.decode function and organized the class slightly ...

--- encodings.lua
+++ encodings.lua
@@ -2,6 +2,17 @@
 -- encodings.lua - (BD 2025) - Playing around with trying to build some cipher encoding / decryption functions ...
 -------------------------------------------
 
+-------------------------------------------
+-- if true then return end -- --- ---- -- 
+-------------------------------------------
+
+------------ ------------ ------------ 
+-- local decs. for speed improvements ...
+------------ ------------ ------------ 
+local cat,insert,sort =
+table.concat,table.insert, table.sort
+------------ ------------ ------------ 
+
 ------- -------- ------- -------- ------
 -- Vigenère Cipher -- --- ------
 ------- -------- ------- -------- ------
@@ -12,11 +23,144 @@
 
 ------- -------- ------- -------- ------
 
+------------ ------------ ------------ 
+-- private (helper) method declarations
+------------ ------------ ------------ 
+local _vigenere, _getCharset, _tokenizeStr 
+------------ ------------ ------------ 
+
+local defaults = {
+  chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+}
+
+--- --- --- --- --- --- --- --- --- (*)
+
+-- vigenere --> holds the Vigenère encoding methods which are exposed for external calls ...
+
+ -- vigenere.encode(txt,key,opt) 
+ -- vigenere.decode(txt,key,opt)
+
+ --[[
+ 
+  -- txt (string) -- text to be encoded
+  -- key (string) -- key to encode with
+
+  -- options: (optional - table): {
+  --- chars: (string)
+  --- keys: (string - regexp) -- allowed keys beyond charaset -- uses Lua lightweight regex i.e '*' or '%s'
+  --- decode: (boolean
+  
+  }
+  
+  -- TODO - opt.chars - could be an expresion or ordered list as either a table or string ...
+  
+--]] 
+
+--- (*) --- --- --- --- --- --- --- ---
+
+local vigenere  = {
+ 
+  --> ------ ------ ------ ------ 
+  -- vigenere.encode(txt,key,options) - converts to an encoded string using a key / cipher and a given charset ...
+  ------ ------ ------ --> 
+  
+  encode = function(txt,key,opt)
+    opt = opt or {}; opt.decode = false
+    return _vigenere(txt,key,opt)
+  end,
+  
+  -- <-- ------ ------ ------ ------ 
+  -- vigenere.decode(txt,key,options) - converts an encoded string to plain text using key / cyper and a given charset ...
+  ------ ------ ------ <--
+  
+  decode = function(txt,key,opt)
+    opt = opt or {}; opt.decode = true
+    return _vigenere(txt,key,opt)
+  end
+  
+  ------ ------ ------ ------
+  
+}
+
+-------- ------ -------- ------- --------
+-- helper functions below this point ...
+-------- ------ -------- ------- --------
+
+-- (helper) - this method is used when the 'vigenere.encode' or 'vigenere.decode' methods are called on a given strin to convert to / from plain text ...
+
+_vigenere = function(txt,key,opt)
+  
+  if not txt then return end
+  if not key then return txt end
+  
+  local charset = opt and opt.chars and opt.chars or defaults.chars
+  local decode = opt and opt.decode
+  
+  local ignoreInvalid = 0
+  if opt and opt.keys and opt.keys then
+    if opt.keys == "*" then ignoreInvalid = 1 
+    elseif type(opt.keys) == "string" then
+      ignoreInvalid = -1
+    end end
+  
+  local cat,insert =
+  table.concat,table.insert
+  
+  ---- ----- ----- -----
+  
+  local set,char = _getCharset(charset)
+  local idx,chars,skipped,delta,_idx = not decode and 1 or set[txt:sub(1,1)], #charset,0,0,0
+  
+  local out = {}
+  
+  for i = 1,#txt do
+    
+    char = txt:sub(i,i)
+    if set[char] then 
+      
+      local keyChar = key:sub(i - skipped,i - skipped)
+      
+      -- skip invalid characters in key / password
+      
+      if ignoreInvalid == 1 or 
+      ignoreInvalid == -1 then
+        
+        while not set[keyChar] do
+          if ignoreInvalid == -1 and not keyChar:match(opt.keys) then break end
+          skipped = skipped + 1
+          keyChar = key:sub(i - skipped, i - skipped)      
+        end end
+      
+      ---- ----- -----
+      
+      delta = set[keyChar] - 1
+      delta = decode and delta * -1 or delta
+      
+      idx = (idx + delta) % chars
+      idx = idx > 0 and idx % chars or
+      chars + idx
+      
+      _idx = (set[char] + delta) % chars
+      _idx = _idx > 0 and _idx % chars or
+      chars + _idx
+      
+      char = charset:sub(_idx,_idx)    
+      
+      ---- ----- -----
+      
+    else skipped = skipped + 1  end
+    
+    insert(out,char) 
+    
+  end;
+  
+return cat(out) end
+
 --- --- --- --- --- --- --- --- --- (*)
 
 -- (helper) -- gets charset table for vigenere. note: character repetitions are parsed out.
 
-local function _getCharset(charset)
+_getCharset = function(charset)
   
   local out,offset,char = {},0
   
@@ -24,97 +168,27 @@ local function _getCharset(charset)
     idx = idx - offset
     char = charset:sub(idx,idx)
     if not out[char] then 
-     out[char] = idx
+      out[char] = idx
     else offset = offset + 1 end
-   end table.sort(out); 
+  end table.sort(out); 
   
-   return out --> returns: table {char:index,...}
+  return out --> returns: table {char:index,...}
 end
 
 --- --- --- --- --- --- --- --- --- (*)
 
-local vigenere = {
-  
-  ------ ------ ------ ------
-  -- vigenere.encode(txt,key,options)
-  -- change text to encoded text with string key / cipher ...
-  
-  --[[
-  
-  -- txt (string) -- text to be encoded
-  -- key (string) -- key to encode with
+-- (helper) breakkes string into a sorted list of characters or expession sets for the sake of character comparison
 
-  -- options: (optional - table): {
-  --- chars: (string)
-  --- keys: (string - regexp) -- allowed keys beyond charaset -- uses Lua lightweight regex i.e '*' or '%s'
+_tokenizeStr = function(str)
   
-  }
   
-  --]]
-  
-  encode = function(txt,key,opt)
-
-   if not txt then return end
-   if not key then return txt end
-  
-   local charset = opt and opt.chars or "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    
-   local ignoreInvalid = 0
-   if opt and opt.keys then
-    if opt.keys == "*" then ignoreInvalid = 1 
-    elseif type(opt.keys) == "string" then
-     ignoreInvalid = -1
-   end end
-      
-   local cat,insert =
-    table.concat,table.insert
-  
-   local idx,skipped,delta,_idx = 1,0,0,0
-   local out = {}
-  
-   local set,char = _getCharset(charset)
-  
-   for i = 1,#txt do
-    
-    char = txt:sub(i,i)
-    if set[char] then 
-      
-     local keyChar = key:sub(i - skipped,i - skipped)
-      
-     -- skip invalid characters in key / password
-        
-     if ignoreInvalid == 1 or 
-        ignoreInvalid == -1 then
-          
-      while not set[keyChar] do
-       if ignoreInvalid == -1 and not keyChar:match(opt.keys) then break end
-       skipped = skipped + 1
-       keyChar = key:sub(i - skipped, i - skipped)      
-      end end
-
-     delta = set[keyChar] - 1
-     idx = (idx + delta) % #charset
-     _idx = (set[char] + delta) % #charset
-     if _idx == 0 then _idx = #charset end
-      char = charset:sub(_idx,_idx)    
-     else skipped = skipped + 1  end
-    
-     insert(out,char) 
-    end;
-        
-  return cat(out) end, 
-  
-  ------- ------ ------ ------ ------
-  
-  decode = function(txt,key)
-    
-  end
-  
-}
+end
 
 -- (*) --- --- --- --- --- --- --- ---  
 
---[[ -- testing first pass code ...
+-------- ------ -------- ------- --------
+--[[ -- testing first pass en(code)ings ...
+-------- ------ -------- ------- --------
 
 -- example input/outpt taken from wikipedia article (see above ...)
 
@@ -126,9 +200,13 @@ local function testVigenereCipher()
 
   print("Testing Vigenère Cipher!")
 
+  local encoded = vigenere.encode(txt,key, {
+    chars = charset })
+  local decoded = vigenere.decode(encoded,
+    key, { chars = charset })
+  
   print("output A: ",
-   vigenere.encode(txt,key, {
-    chars = charset }))
+   encoded,decoded )
   
   print("output B: ",
    vigenere.encode(txt,key))
@@ -149,3 +227,7 @@ end
 ----- ----------- ----------- -----------  
 return vigenere --> --- ---- -----
 ----- ----------- ----------- -----------  
+
+-------- ------ -------- ------- --------
+-- {{ File End - encodings.lua }}
+-------- ------ -------- ------- --------
